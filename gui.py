0561c0887afde5d1756f5e8f27437b725cf9947b
OlympusMonds <luke.s.mondy@gmail.com>
2015-02-22 21:55:06 +1100

    OK, major update to GUI. Switched to Kivy. Current state is that the front-end and back-end are essentially unlinked

--- gui.py
+++ gui.py
@@ -1,41 +1,104 @@
-from Tkinter import Tk, BOTH
-from ttk import Frame, Button, Style
-from functools import partial
+from math import sin, cos, radians
+from collections import defaultdict
 
-class MainGUI(Frame):
-
-	def __init__(self, parent):
-		Frame.__init__(self, parent)
-
-		self.parent = parent
-		self.initUI()
-		
-		self.communicators = {}
-		self.buttons = {}
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.uix.floatlayout import FloatLayout
+from kivy.properties import ObjectProperty, NumericProperty
+from kivy.graphics import Line, Ellipse, Color
+from kivy.clock import Clock
+from kivy.animation import Animation
+from kivy.factory import Factory
 
 
-	def initUI(self):
-		self.parent.title("PyDelivery")
-		self.style = Style()
-		self.style.theme_use("default")
+class PyDelMainScreen(Widget):
+    
+    image_radius = NumericProperty(50)
+    rad = NumericProperty(50)
+    rad_opac = NumericProperty(1.0)
 
-		self.pack(fill=BOTH, expand=1)
+    disp_count = 0
 
-		quitButton = Button(self, text="Quit", command=self.quit)
-		quitButton.place(x=50, y=50)
+    peers = defaultdict(dict)
+    outside_peers = {"Kayla": {"icon" : "images/peer.png",},
+                     #"Ed":    {"icon" : "images/peer.png",},
+                     #"Nath":  {"icon" : "images/peer.png",},
+                     #"Ben":   {"icon" : "images/peer.png",},
+                    }
+ # This is a stub
+   
+    def __init__(self, **kwargs):
+        super(PyDelMainScreen, self).__init__(**kwargs)
+        # Just in case
 
 
-	def delete_communicator(self, name):
-		del self.communicators[name]
-		self.buttons[name].destroy()
-		del self.buttons[name]
+    def update_radar(self, dt):
+        max_rad = max(self.width / 2.0,  (self.height - (self.height / 10.0)))
+        self.rad += 1
+        self.rad_opac = 1.0 - (self.rad / max_rad)
+        
+        if self.rad > max_rad:
+            self.rad = self.image_radius
 
 
-	def add_communicator(self, comm):
-		if comm.name not in self.communicators.keys():
-			self.communicators[comm.name] = comm
-			commButton = Button(self, text=comm.name, command=partial(self.delete_communicator, comm.name))
-			commButton.place(x=50, y=len(self.communicators.keys())*50+50)
-			self.buttons[comm.name] = commButton
-		else:
-			pass
+    def update_peers(self, dt):
+        self.disp_count += 1
+
+        if self.disp_count % 40 == 0:
+            self.outside_peers["name%d" % self.disp_count] = {"icon": "images/peer.png"}
+
+
+        for name, val in self.outside_peers.iteritems():
+            if name in self.peers.keys():
+                continue  # Already added to gui 
+            else:
+                with self.fl.canvas:
+                    self.peers[name]["peer_obj"] = Factory.Peer()  # Add a new peer
+
+        self.peer_layout()  # Set peer locations
+                
+
+    def peer_layout(self):
+        num_peers = len(self.outside_peers.keys())
+        
+        radar_rad = min(self.width, self.height) / 2.0
+        radar_rad -= 50  # border buffer
+        semi_circ_segment = 180.0 / (num_peers + 1)
+
+        count = 0
+        for name in self.peers.keys():
+            count += 1
+            peer = self.peers[name]["peer_obj"]
+
+            xpos = radar_rad * cos(radians(semi_circ_segment * count))
+            ypos = radar_rad * sin(radians(semi_circ_segment * count))
+
+            pos = (int(self.center_x + xpos - self.image_radius), 
+                   int((self.height / 10.0) + ypos))
+
+            peer.source = "images/peer.png"
+            peer.size = (self.image_radius * 2, self.image_radius * 2)
+            
+            need_animation = False
+            if pos != peer.pos:
+                if "ani_obj" not in self.peers[name].keys():
+                    need_animation = True
+                    # This if should protect against Key_Error exception
+                elif not self.peers[name]["ani_obj"].have_properties_to_animate(peer):
+                    need_animation = True
+
+                if need_animation:
+                    self.peers[name]["ani_obj"] = Animation(pos = pos, duration = 0.5, t='in_out_circ')
+                    self.peers[name]["ani_obj"].start(peer)
+
+
+
+class PyDelApp(App):
+    
+    def build(self):
+        mainscreen = PyDelMainScreen()
+        Clock.schedule_interval(mainscreen.update_radar, 1.0/60.0)
+        Clock.schedule_interval(mainscreen.update_peers, 0.25)
+        return mainscreen
+
--- pydelivery.py
+++ pydelivery.py
@@ -15,7 +15,7 @@ from zeroconf import ServiceBrowser, Zeroconf
 from zc_annoucers import ZConfAnnoucer, ZConfListener
 
 from FileSender import FileSender
-from gui import MainGUI
+from gui import PyDelApp
 
 from communication import DataReceiver, DataServer
 import threading
@@ -24,90 +24,81 @@ import argparse
 from sendfile import sendfile
 
 def main():
-	"""
-	Main entry point
-	"""
+    """
+    Main entry point
+    """
 
-	# Parse any command-line args (I will add to these)
-	parser = argparse.ArgumentParser()
-	parser.add_argument("--test-as-listener", action = "store_true", default = True)
-	parser.add_argument("--test-as-sender", action = "store_true", default = False)
-	args = parser.parse_args()
-	
-	check_for_people_interval = 2000
-
-	
-	# Get to the main busines
-
-	zconf = Zeroconf()
-	try:
-		# Bit of a hack to get local IP - see http://stackoverflow.com/a/166589/1728112
-		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		s.connect(("129.1.1.1",80)) # The address can be garbage - it just has to NOT be local 
-		localip = (s.getsockname()[0])
-		s.close()
-	except Exception as e:
-		sys.exit("Unable to determine your local IP address. Computer says:\n{}".format(e))
-	
-	# Step X - bind a port to listen on
-	address = (localip,  0) # Let the OS choose a port
-	dataserver = DataServer(address, DataReceiver)
-	ip, port = dataserver.server_address
-
-
-	# Step X - annouce yourself on the network:
-	hostname = socket.gethostname()
-	properties = {"Nice name" : "Luke's VM"}
-	zcannoucer = ZConfAnnoucer(zconf, "_http._tcp.", hostname,
-			                 localip, port, properties = properties)
-
-	# Step X - listen for anyone else annoucing:
-	zclistener = ZConfListener()
-	zcbrowser = ServiceBrowser(zconf, "_http._tcp.", zclistener)
-
-	dataserver.set_peers(zclistener.peers)
-
-	dataserver_thread = threading.Thread(target=dataserver.serve_forever)
-	dataserver_thread.daemon = True
-	dataserver_thread.start()
+    # Parse any command-line args (I will add to these)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--test-as-listener", action = "store_true", default = True)
+    parser.add_argument("--test-as-sender", action = "store_true", default = False)
+    args = parser.parse_args()
     
-        file_to_send = "test_file.zip"
+    check_for_people_interval = 2000
 
-	if args.test_as_sender:
-		time.sleep(5)
-		print("Peers!")
-		for name, comm in zclistener.peers.iteritems():
-			if hostname not in name: # Not localhost
-				print("Trying to send file to {}:{}".format(comm.address, comm.port))
-				try:
-					sock = socket.socket()
-					sock.connect((comm.address, comm.port))
-					sock.sendall("receive file:{}".format(file_to_send))
-					response = sock.recv(1024)
-					print("Response from {}: {}".format(comm.address, response))
-					if "OK" in response:
-						print("got an OK")
-						fs = FileSender("test_file.zip", sock)
-					else:
-						print("Not OK to send")
-				finally:
-					sock.close()
-		print("Done")
-	else:
-		# Step 3 - start up the GUI, and monitor the situation
-		root=Tk()
-		root.geometry("250x250+300+300")
-		maingui = MainGUI(root)
+    
+    # Get to the main busines
 
-		def task():
-			if len(zclistener) > 0:
-				for key, val in zclistener.peers.iteritems():
-					maingui.add_communicator(val)
-			root.after(check_for_people_interval, task)  # reschedule event in 1 seconds
+    zconf = Zeroconf()
+    try:
+        # Bit of a hack to get local IP - see http://stackoverflow.com/a/166589/1728112
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("129.1.1.1",80)) # The address can be garbage - it just has to NOT be local 
+        localip = (s.getsockname()[0])
+        s.close()
+    except Exception as e:
+        sys.exit("Unable to determine your local IP address. Computer says:\n{}".format(e))
+    
+    # Step X - bind a port to listen on
+    address = (localip,  0) # Let the OS choose a port
+    dataserver = DataServer(address, DataReceiver)
+    ip, port = dataserver.server_address
 
-		root.after(check_for_people_interval, task)
-		root.mainloop()
+
+    # Step X - annouce yourself on the network:
+    hostname = socket.gethostname()
+    properties = {"Nice name" : "Luke's VM"}
+    zcannoucer = ZConfAnnoucer(zconf, "_http._tcp.", hostname,
+                             localip, port, properties = properties)
+
+    # Step X - listen for anyone else annoucing:
+    zclistener = ZConfListener()
+    zcbrowser = ServiceBrowser(zconf, "_http._tcp.", zclistener)
+
+    dataserver.set_peers(zclistener.peers)
+
+    dataserver_thread = threading.Thread(target=dataserver.serve_forever)
+    dataserver_thread.daemon = True
+    dataserver_thread.start()
+    
+    file_to_send = "test_file.zip"
+
+    if args.test_as_sender:
+        time.sleep(5)
+        print("Peers!")
+        for name, comm in zclistener.peers.iteritems():
+            if hostname not in name: # Not localhost
+                print("Trying to send file to {}:{}".format(comm.address, comm.port))
+                try:
+                    sock = socket.socket()
+                    sock.connect((comm.address, comm.port))
+                    sock.sendall("receive file:{}".format(file_to_send))
+                    response = sock.recv(1024)
+                    print("Response from {}: {}".format(comm.address, response))
+                    if "OK" in response:
+                        print("got an OK")
+                        fs = FileSender("test_file.zip", sock)
+                    else:
+                        print("Not OK to send")
+                finally:
+                    sock.close()
+        print("Done")
+    else:
+        # Step 3 - start up the GUI, and monitor the situation
+        maingui = PyDelApp()
+        maingui.run()
+
 
 if __name__ == "__main__":
-	sys.exit(main())
+    sys.exit(main())
 
